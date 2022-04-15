7d6d22c3bccd137afb5d6038f66c643cb96d8d85
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-15 22:26:58 +0200

    [+] desactivation de la colision au moment du dash fakhear

--- src/server/game/scenes/Aptitudes/fakhear.ts
+++ src/server/game/scenes/Aptitudes/fakhear.ts
@@ -12,12 +12,14 @@ export function kick(fakhear: Phaser.Physics.Arcade.Sprite|any) {
 export function dash(fakhear: Phaser.Physics.Arcade.Sprite|any) {
   fakhear.play('straightlead')
   fakhear.setVelocityX(0);
+  fakhear.body.checkCollision.none = true;
+
   fakhear.scene.tweens.addCounter({
     from: 0,
     to: 1,
     duration: 300,
     onUpdate: () => (fakhear.setVelocity((fakhear.flipX ? -1700 : 1700), -70)),
-    onComplete: () => (fakhear.setVelocityX(0), fakhear.play('idle_attack')),
+    onComplete: () => (fakhear.setVelocityX(0), fakhear.play('idle_attack'), fakhear.body.checkCollision.none = false),
     repeat: 0,            // -1: infinity
     yoyo: false,
   })
