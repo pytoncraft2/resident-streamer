294690ae532d9787c8ddea0a0c9e8f9cb064120a
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-16 18:52:24 +0200

    [~] test de la réinitialisation de la position

--- src/server/game/scenes/Aptitudes/huzounet.ts
+++ src/server/game/scenes/Aptitudes/huzounet.ts
@@ -5,6 +5,7 @@ export function kunai(huzounet:Phaser.Physics.Arcade.Sprite|any) {
 }
 
 export function shuriken(huzounet: Phaser.Physics.Arcade.Sprite|any) {
+  let tween;
   if (huzounet.scene.groupeBoules.getLength() == 0) {
     setAnimation(huzounet, 'attack')
     huzounet.scene.room.broadcast("apparition-boule", {x: huzounet.x, y: huzounet.y, id: huzounet.ClientID})
@@ -12,14 +13,15 @@ export function shuriken(huzounet: Phaser.Physics.Arcade.Sprite|any) {
     const boulePhysique = huzounet.scene.physics.add.existing(boule)
     // huzounet.scene.physics.world.enable(boulePhysique);
     // huzounet.scene.physics.add.collider(boulePhysique, huzounet.scene.enemies);
-    if (boulePhysique.body) {
-      huzounet.scene.tweens.addCounter({
+      huzounet.lancer = huzounet.scene.tweens.addCounter({
         duration: 1000,
         onUpdate: () => (huzounet.anims.getFrameName() == 'positiona4' && boulePhysique.body && boulePhysique.body.setVelocityX(huzounet.flipX ? -1900 : 1900)),
-        // onComplete: () => (huzounet.scene.groupeBoules.clear()),
+        onComplete: () => (huzounet.scene.groupeBoules.setXY(huzounet.x, huzounet.y)),
         repeat: 0,
         yoyo: false,
       })
-    }
+  } else if (huzounet.scene.groupeBoules.getLength() == 1) {
+    huzounet.scene.groupeBoules.setXY(huzounet.x, huzounet.y)
+    huzounet.lancer.restart()
   }
 }
