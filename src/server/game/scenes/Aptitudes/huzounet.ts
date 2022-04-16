34965bd340c1192ce81fc8699ae3647e9335890f
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-16 17:32:52 +0200

    [+] lancé de shurikan amélioration

--- src/server/game/scenes/Aptitudes/huzounet.ts
+++ src/server/game/scenes/Aptitudes/huzounet.ts
@@ -5,9 +5,8 @@ export function kunai(huzounet:Phaser.Physics.Arcade.Sprite|any) {
 }
 
 export function shuriken(huzounet) {
+  if (huzounet.scene.groupeBoules.getLength() == 0) {
   setAnimation(huzounet, 'attack')
-  var coefDir = -1;
-
   huzounet.scene.room.broadcast("apparition-boule", {x: huzounet.x, y: huzounet.y, id: huzounet.ClientID})
   const boule = huzounet.scene.groupeBoules.create(huzounet.x, huzounet.y - 4, `atlas`, 'shuriken0')
   const boulePhysique = huzounet.scene.physics.add.existing(boule)
@@ -17,10 +16,11 @@ export function shuriken(huzounet) {
   // if (!huzounet.lancer) {
     huzounet.scene.tweens.addCounter({
       duration: 1000,
-      onUpdate: () => (huzounet.anims.getFrameName() == 'positiona4' ? boulePhysique.body.setVelocityX(huzounet.flipX ? -1900 : 1900) : huzounet.setVelocityX(0)),
+      onUpdate: () => (huzounet.anims.getFrameName() == 'positiona4' && boulePhysique.body.setVelocityX(huzounet.flipX ? -1900 : 1900)),
       onComplete: () => (huzounet.scene.groupeBoules.clear()),
       repeat: 0,            // -1: infinity
       yoyo: false,
     })
+  }
   // }
 }
