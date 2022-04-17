be42c5ba261bf77ca26a702710353d6f3b2fbed9
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-17 22:05:31 +0200

    [💡] création d'une class exclusivement pour la boule

---
+++ src/server/game/scenes/class/Boule.ts
@@ -0,0 +1,32 @@
+export default class PlayerClass extends Phaser.Physics.Arcade.Sprite {
+  vel: number = 400
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    sprite: string,
+    ClientID: string
+  ) {
+    super(scene, x, y, sprite)
+
+    this.init(scene, ClientID)
+  }
+
+  init(scene: Phaser.Scene, ClientID: string) {
+    this.scene = scene
+    this.scene.add.existing(this)
+    this.ClientID = ClientID
+  }
+  preUpdate(time, delta) {
+    // console.log(this.anims.msPerFrame += 300)
+    super.preUpdate(time, delta);
+
+    (this.scene as any).room.state.presences.set(
+      this.ClientID,
+      new Boule({
+        x: this.x,
+        y: this.y,
+      })
+    )
+  }
+}
