9b22373f8e517b4ed1ddedb5faac87ad9388c806
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-17 02:55:27 +0200

    [⚙️] recuperer la puissance de l'envoie d'attaque

--- src/server/game/scenes/Aptitudes/huzounet.ts
+++ src/server/game/scenes/Aptitudes/huzounet.ts
@@ -82,6 +82,8 @@ export function shuriken(huzounet:Phaser.Physics.Arcade.Sprite|any, input?: any)
       setAnimation(huzounet, 'huzounet_preparation_attaque')
 
       huzounet.animationCharge = huzounet.scene.tweens.add({
+        from: 0,
+        to: 10,
         targets: huzounet.boulePhysique,
         scale: '+=2',
         duration: 3000
@@ -90,8 +92,10 @@ export function shuriken(huzounet:Phaser.Physics.Arcade.Sprite|any, input?: any)
       input.a.charge = false
     }
     if (input.a.envoie) {
-      console.log(huzounet.parametresDeBase)
+      // console.log(huzounet.parametresDeBase)
       setAnimation(huzounet, 'huzounet_envoie_attaque')
+      console.log("VVVVVVVVVVVVVVVVVVVVVVVVALUE")
+      console.log(huzounet.animationCharge.progress)
       huzounet.animationCharge.remove()
       huzounet.boulePhysique.body && huzounet.boulePhysique.body.setVelocityX(huzounet.flipX ? -2400 : 2400)
       // console.log("EENNNNVOIE")
