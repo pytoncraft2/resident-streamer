981cfbab64bf75d4cc01d8ccdf9cea1dac0c24d0
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-18 01:37:02 +0200

    [~] test changement scale coté serveur

--- src/server/game/scenes/Aptitudes/huzounet.ts
+++ src/server/game/scenes/Aptitudes/huzounet.ts
@@ -8,11 +8,19 @@ export function shuriken(huzounet:Phaser.Physics.Arcade.Sprite|any, input?: any)
     huzounet.scene.groupeBoules.add(huzounet.boule)
     // console.log(huzounet.scene.groupeBoules.getLength())
     // huzounet.boule.setVelocityX(huzounet.flipX ? -2400 : 2400)
+    huzounet.animationCharge = huzounet.scene.tweens.add({
+      targets: huzounet.boule,
+      scale: 2,
+      duration: 3000
+    });
     input.a.charge = false
   }
   if (input.a.envoie) {
     input.a.envoie = false
   }
+    console.log(huzounet.boule.progress)
+  input.a = undefined
+  console.log("BOOOUCLE")
 }
 
 export function kunai(huzounet: Phaser.Physics.Arcade.Sprite|any) {
