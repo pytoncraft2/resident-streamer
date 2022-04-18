3e1b78d73b8080c17e58e67e9a7d501ce965d61e
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-18 21:11:09 +0200

    [~] FIXME gestion charge et décharge de la boule

--- src/server/game/scenes/Aptitudes/huzounet.ts
+++ src/server/game/scenes/Aptitudes/huzounet.ts
@@ -7,18 +7,6 @@ export function shuriken(huzounet:Phaser.Physics.Arcade.Sprite|any, input?: any)
     huzounet.boule = huzounet.scene.add.existing(new BouleClass(huzounet.scene, huzounet.x, huzounet.y, "atlas",  `${(Math.random() + 1).toString(36).substring(7)}`).setData({ ClientId: huzounet.ClientID}))
     huzounet.scene.groupeBoules.add(huzounet.boule)
 
-    // if (!huzounet.animationCharge) {
-    //   huzounet.animationCharge = huzounet.scene.tweens.add({
-    //     targets: huzounet.boule,
-    //     scale: 2,
-    //     duration: 3000,
-    //     paused: true
-    //   });
-    // }
-    //
-    // if (huzounet.animationCharge.isPaused()) {
-    //   huzounet.animationCharge.play()
-    // }
     setAnimation(huzounet, 'huzounet_preparation_attaque')
 
     input.a.charge = false
@@ -27,22 +15,12 @@ export function shuriken(huzounet:Phaser.Physics.Arcade.Sprite|any, input?: any)
   if (input.a.envoie) {
 
     huzounet.boule.stopAnim()
-    // huzounet.animationCharge.stop()
 
-    // huzounet.boule.setData({puissance: huzounet.animationCharge.progress})
-
-    if (huzounet.boule.body) {
-      setAnimation(huzounet, 'huzounet_envoie_attaque')
-      huzounet.boule.body.setVelocityX(huzounet.flipX ? -2400 : 2400)
-      
-    }
+    setAnimation(huzounet, 'huzounet_envoie_attaque')
+    huzounet.boule.body.setVelocityX(huzounet.flipX ? -2400 : 2400)
 
     input.a.envoie = false
   }
-
-  // function stopAnimation(huzounet) {
-  // huzounet.anima
-  // }
 }
 
 export function kunai(huzounet: Phaser.Physics.Arcade.Sprite|any) {
