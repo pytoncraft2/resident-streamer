c957818cf8dce9ceda8e4447db0340531dbecf5d
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-22 22:47:54 +0200

    [~] grossisement de la boule

--- src/server/game/scenes/Aptitudes/huzounet.ts
+++ src/server/game/scenes/Aptitudes/huzounet.ts
@@ -6,11 +6,25 @@ import TJoueur from "../types/Joueur";
 
 export function shuriken(huzounet: TJoueur, input?: any) {
 
-  // console.log("charge")
-  // @ts-ignore
-  // huzounet.charge = true
+  if (input.a) {
+    //creation de la boule si non créer
+    if (!huzounet.scene.groupeBoulesHuzounet.getLength())
+    {
+      console.log("CREATION BOULE")
+      const boule = huzounet.scene.add.existing(new BouleClass(huzounet.scene, huzounet.x -80, huzounet.y - 160, "atlas",  `${(Math.random() + 1).toString(36).substring(7)}`).setData({ ClientId: huzounet.ClientID, puissance: 2}))
+      huzounet.scene.groupeBoulesHuzounet.add(boule);
+      (boule.body as any).setAllowGravity(false);
 
-  if (input.a) console.log("A")
+
+    //grossisement de la boule
+    } else 
+    {
+      console.log("GROSSISEMENT");
+      (huzounet.scene.groupeBoulesHuzounet.getChildren()[0] as Phaser.Physics.Arcade.Sprite).scale += 0.01;
+      (huzounet.scene.groupeBoulesHuzounet.getChildren()[0] as Phaser.Physics.Arcade.Sprite).alpha += 0.01;
+    }
+    // console.log("A")
+  }
   if (input.a_fin) (console.log("FIN A"), input.a_fin = false)
   // huzounet.x += 1
   // if (input.a.charge)
