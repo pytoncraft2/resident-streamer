960041c0b960a023c77efe60cfc4da1aa9c9cd51
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-23 02:11:37 +0200

    [🔥] envoie de plusieurs boules

--- src/server/game/scenes/Aptitudes/huzounet.ts
+++ src/server/game/scenes/Aptitudes/huzounet.ts
@@ -22,7 +22,6 @@ export function shuriken(huzounet: TJoueur, input?: any) {
       huzounet.puissanceChargeBoule = 0
       const boule = huzounet.scene.add.existing(new BouleClass(huzounet.scene, huzounet.x -80, huzounet.y - 160, "atlas",  `${(Math.random() + 1).toString(36).substring(7)}`).setData({ ClientId: huzounet.ClientID, puissance: 2}))
       huzounet.parametresDeBase.boulesEnMain.add(boule);
-      boule.proprietaire = huzounet.ClientID;
       (boule.body as any).setAllowGravity(false);
 
       setAnimation(huzounet, 'huzounet_preparation_attaque')
@@ -44,9 +43,10 @@ export function shuriken(huzounet: TJoueur, input?: any) {
       console.log("ENVOIE");
 
       const boule = (huzounet.parametresDeBase.boulesEnMain.getChildren()[0] as BouleClass);
+      boule.proprietaire = huzounet.ClientID;
       huzounet.scene.groupeBoulesHuzounet.add(boule.setData('degat', huzounet.puissanceChargeBoule));
-      //@ts-ignore
-      huzounet.scene.groupeBoulesHuzounet.getMatching('proprietaire', huzounet.ClientID)[0].body.setVelocityX(2600).proprietaire = '';
+      huzounet.scene.groupeBoulesHuzounet.getMatching('proprietaire', huzounet.ClientID)[0].body.setVelocityX(2600);
+      huzounet.scene.groupeBoulesHuzounet.getMatching('proprietaire', huzounet.ClientID)[0].proprietaire = '';
       // huzounet.scene.groupeBoulesHuzounet.getMatching('proprietaire', huzounet.ClientID)[0].setVelocityX(2600);
       huzounet.parametresDeBase.boulesEnMain.clear();
       input.a_fin = false
