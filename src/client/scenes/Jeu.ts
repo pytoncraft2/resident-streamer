d0ce26c7c1aec73a22844ea45f5fd2887611a0b2
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-24 23:22:34 +0200

    [⚙️] animation idle_walk

--- src/client/scenes/Jeu.ts
+++ src/client/scenes/Jeu.ts
@@ -657,9 +657,9 @@ export default class Jeu extends Phaser.Scene {
         r: R.isDown ? true : false,
         space: space.isDown ? true : false,
         right: right.isDown ? true : false,
-        right_fin: Phaser.Input.Keyboard.JustUp(right) ? true : false,
+        right_fin: right.isUp,
         left: left.isDown ? true : false,
-        left_fin: Phaser.Input.Keyboard.JustUp(left) ? true : false
+        left_fin: left.isUp
       }
 
       //ATTAQUE
