402ea6c3d91aece8dbd8c79913a76ffedbbaf238
pytoncraft2 <pythoncraft2@gmail.com>
2022-04-30 23:20:56 +0200

    [~] attaque de manette à la verticale

--- src/server/game/scenes/Aptitudes/_Aptitudes_bosses/manette.ts
+++ src/server/game/scenes/Aptitudes/_Aptitudes_bosses/manette.ts
@@ -7,22 +7,27 @@ export function punch(manette, input) {
 
 export function lanceManette(manette, input) {
 
-  if (!manette.kunai) {
+  if (!manette.obj_manette) {
     manette.play('manette_lance')
-    const kunai = manette.scene.add.existing(new ManetteClass(manette.scene, manette.flipX ? manette.x - 80 : manette.x + 80, manette.y - 60, "manette",  `${(Math.random() + 1).toString(36).substring(7)}`)
+    const obj_manette = manette.scene.add.existing(new ManetteClass(manette.scene, manette.flipX ? manette.x - 80 : manette.x + 80, manette.y - 60, "manette",  `${(Math.random() + 1).toString(36).substring(7)}`)
     .setData({ ClientId: manette.ClientID, degat: manette.degat}))
     .setFlipX(manette.flipX)
-    manette.scene.physics.add.existing(kunai);
-    manette.scene.physics.add.overlap(kunai, (manette.scene as any).enemies, function(_kunai, _ennemie: any) {
-      _ennemie.blesse_ennemie(_kunai.getData('degat'))
-      _kunai.setData('degat', 0)
+    manette.scene.physics.add.existing(obj_manette);
+    manette.scene.physics.add.overlap(obj_manette, (manette.scene as any).enemies, function(_obj_manette, _ennemie: any) {
+      _ennemie.blesse_ennemie(_obj_manette.getData('degat'))
+      _obj_manette.setData('degat', 0)
     }, undefined, manette);
 
-    (kunai.body as any).setAllowGravity(false);
-    manette.kunai = kunai
+    (obj_manette.body as any).setAllowGravity(false);
+    manette.obj_manette = obj_manette
     manette.scene.time.delayedCall(100, () => {
-      manette.kunai.setVelocityX(manette.flipX ? -2300 : 2300).setFlipX(manette.flipX)
-      manette.kunai = undefined;
+      if (manette.anims.getName() == "manette_vole") {
+        manette.obj_manette.setVelocityY(2300).setFlipX(manette.flipX)
+      } else {
+        manette.obj_manette.setVelocityX(manette.flipX ? -2300 : 2300).setFlipX(manette.flipX)
+      }
+
+      manette.obj_manette = undefined;
     }, null, manette);
   }
 }
