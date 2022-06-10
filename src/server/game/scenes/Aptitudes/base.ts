0550d5bede48b89403c6a4a4e5dd97e58bebf22c
pytoncraft2 <pythoncraft2@gmail.com>
2022-06-10 23:27:53 +0200

    [~] passage de l'objet test en objet Aptitudes[personnage]

--- src/server/game/scenes/Aptitudes/base.ts
+++ src/server/game/scenes/Aptitudes/base.ts
@@ -13,36 +13,22 @@ fs.readdir('./src/server/game/scenes/Aptitudes', (_err, files) => {
       {
         //capture d'un tableau
         Object.values(m).forEach((element, i) => {
-          //capture de tout les element d'un tableau (fichier)
-          // const sym1 = element
-          // const o = {
-            // [sym1]: element
-          // };
-
           let index = element.toString().split(' ')[1].indexOf("__")
           if (index != -1) {
             let CLE = element.toString().split(' ')[1].substr(index+2).split('(')[0]
             const personnage = file.substring(0, file.lastIndexOf('.'))
-
-            if (lesTouches[personnage] === undefined) lesTouches[personnage] = {}
-            lesTouches[personnage]['touche'+CLE] = element
-            // lesTouches[personnage][CLE] = element
-            // console.log(file.substring(0, file.lastIndexOf('.')))
+            if (Aptitudes[personnage] === undefined) Aptitudes[personnage] = {}
+            Aptitudes[personnage]['touche'+CLE] = element
           }
-          let underscoreRecherche = element.toString().split(' ')[1].substr(index+2)[0]
-
-            // console.log("INDEX:", i)
-            touche[`${underscoreRecherche}`] = Object.values(m)[i]
-          // toString()[0]
         })
-        Aptitudes[file.substring(0, file.lastIndexOf('.'))] = {
-          stats: Object.values(m)[0],
-          toucheA: Object.values(m)[1],
-          toucheZ: Object.values(m)[2],
-          toucheE: Object.values(m)[3],
-          toucheR: Object.values(m)[4],
-          toucheEspace: Object.values(m)[5]
-        }
+        // Aptitudes[file.substring(0, file.lastIndexOf('.'))] = {
+        //   stats: Object.values(m)[0],
+        //   toucheA: Object.values(m)[1],
+        //   toucheZ: Object.values(m)[2],
+        //   toucheE: Object.values(m)[3],
+        //   toucheR: Object.values(m)[4],
+        //   toucheEspace: Object.values(m)[5]
+        // }
 
       }
     });
