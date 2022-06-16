788dc6c244ea3ce06fa9e6db35b15a8d115e917f
pytoncraft2 <pythoncraft2@gmail.com>
2022-06-17 00:39:23 +0200

    [Objectif] : creer une logique pour les bot de cycle d'attaque

--- src/server/game/scenes/hall.ts
+++ src/server/game/scenes/hall.ts
@@ -154,13 +154,6 @@ this.physics.add.overlap(this.ennemieAttackZone, [this.players], this.overlapAct
   createEnnemy(EnnemyId: any, sprite: string) {
     // const ennemy = new EnnemyClass(this, 3539, 706, "atlas", EnnemyId).setData({ EnnemyId })
     const ennemy = this.add.existing(new PlayerClass(this, 1000, 0, "atlas", EnnemyId, sprite).setData({ EnnemyId }))
-    ennemy.texture.source[0].source = []
-    this.time.delayedCall(4000, () => {
-      console.log(ennemy.texture.source[0])
-      console.log(ennemy.texture.source[0].source)
-    }, null, this);
-
-    ennemy.texture.source[0].source = []
     // ennemy.setPushable(false)
     // ennemy.setDisplaySize(335, 540.4)
     // ennemy.setBounceX(40)
