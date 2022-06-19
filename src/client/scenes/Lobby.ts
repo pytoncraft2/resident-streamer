2db924842bbdb15fa313a870bb620e4bb818b18f
pytoncraft2 <pythoncraft2@gmail.com>
2022-06-19 03:30:37 +0200

    [~] bug interstellaire lobby multiple sprite

--- src/client/scenes/Lobby.ts
+++ src/client/scenes/Lobby.ts
@@ -61,56 +61,232 @@ export default class Lobby extends Phaser.Scene {
   }
 
 
-	async create() {
-		this.editorCreate();
-		this.boutonLobby
-		.setInteractive({ useHandCursor: true })
-		.on('pointerdown', () => {
-			this.scene.start('Jeu')
-		});
+	  copieUrl() {
+	    let url = `${window.location.protocol}//${document.location.hostname}:${document.location.port}/${this.salon}`
+	    navigator.clipboard.writeText(url).then(function() {
+	      console.log(`${url} copié!`);
+	    }, function() {
+	      console.log(`${url} non copié!`);
+	    });
+	  }
 
-		console.log(`WELCOME TO LOBBY ${this.salon}`)
 
-		await this.connexion()
-	}
 
-	async connexion() {
-		const self = this
-		const client = new Colyseus.Client("ws://localhost:3000")
-		const salon = this.salon
+	  /**
+	   * async create - Creation et Initialisation des textes, liste d'images de personnages
+	   * et activation de l'ineraction avec Phaser
+	   */
+	  async create() {
+			this.editorCreate();
 
-console.log("LLLLOOOBY")
-		await client
-		.joinOrCreate("lobby", { salon })
-		.then((room) => {
-			self.room = room
-			self.session = room.sessionId
+	    const self = this;
 
-			setTimeout(() => {
-				// self.demandeCommencerJeu()
-			}, 2000);
+	    this.panelGauche = new Panel("JOUEURS: 0/4",['Selectionner un personnage !'], this, () => {})
 
-			room.onMessage("commencerJeu", _message => {
-				self.commencerJeu()
-			});
-		})
-		.catch((err) => {
-			console.error(err)
-		})
-	}
+	    this.listeJoueur = {
+	      0: self.add.text(645, 689 , [''], { fontFamily: 'CustomFontNormal' }).setFontSize(25).setAlpha(0.5).setOrigin(0.5).setDepth(3),
+	      1: self.add.text(845, 689 , [''], { fontFamily: 'CustomFontNormal' }).setFontSize(25).setAlpha(0.5).setOrigin(0.5).setDepth(3),
+	      2: self.add.text(1045, 689 , [''], { fontFamily: 'CustomFontNormal' }).setFontSize(25).setAlpha(0.5).setOrigin(0.5).setDepth(3),
+	      3: self.add.text(1245, 689, [''], { fontFamily: 'CustomFontNormal' }).setFontSize(25).setAlpha(0.5).setOrigin(0.5).setDepth(3)
+	    }
 
-	commencerJeu() {
-		this.room.leave()
-		this.scene.start('Jeu', {
-			salon: this.salon,
-			personnage: this.personnageChoisie
-		});
-	}
+	    new Titre(window.innerWidth/2, 100, `Lobby : ${this.salon}`, this, () => this.copieUrl())
 
-	demandeCommencerJeu() {
-		this.room.send('demandeCommencerJeu')
-	}
+	    this.bouton = new Button(window.innerWidth / 2, window.innerHeight - 100, 'Selectionner votre personnage !', this, () => {
+	      if (self.boutonActivable) {
+	        self.demandeCommencerJeu()
+	      }
+	    });
 
+	    const listeIndex: any = []
+
+	    this.container = this.add.container(645, 0);
+	    await this.connexion()
+
+	    this.personnages.forEach((element, idx) => {
+	      const img = self.add.image(0 + idx * 200, this.cameras.main.centerY, element)
+	      .setData('actif', false)
+	      .setAlpha(0.8)
+	      .setInteractive(({ useHandCursor: true }))
+	      .on('pointerdown', function() {
+	        listeIndex.push(idx)
+					//@ts-ignore
+	        self.personnageChoisie = `${this.frame.texture.key}`
+	        self.container.iterate(function(el: any) {
+	              el.setAlpha(0.3)
+	              el.setData('actif', false)
+	        });
+
+	        self.room.send('etatJoueur', {
+	          pret: true,
+	          indexConfirmation: idx,
+	          ancienIndexConfirmation: listeIndex[listeIndex.length - 2]
+	        })
+					//@ts-ignore
+	        this.setData('actif', true)
+					//@ts-ignore
+	        this.setAlpha(1)
+	        ellipse.setAlpha(0.6)
+	        ellipse.setData('actif', true)
+	      }).on('pointerover', function() {
+					//@ts-ignore
+	        if (!this.getData('actif')) {
+					//@ts-ignore
+	          this.setAlpha(1)
+	          ellipse.setAlpha(0.6)
+	        }
+	      }).on('pointerout', function() {
+					//@ts-ignore
+	        if (!this.getData('actif')) {
+					//@ts-ignore
+	          this.setAlpha(0.6)
+	          ellipse.setAlpha(0.3)
+	        }
+	      })
+	      const ellipse = self.add.ellipse(img.x, img.y + img.displayHeight / 2 - 10, 200, 35, 0x00000).setAlpha(0.3).setDepth(-1);
+	      this.container.add([img, ellipse])
+	    });
+
+	  }
+
+
+
+	  /**
+	   * async connexion - Connexion au Lobby et mise à jour des donnes selon les changement du serveur
+	   */
+	  async connexion() {
+	    const self = this
+	    this.client = new Colyseus.Client("ws://localhost:3000")
+	    const salon = this.salon
+	    const client = this.client
+
+
+	    await client
+	    .joinOrCreate("lobby", { salon })
+	    .then((room) => {
+	      self.room = room
+	      self.session = room.sessionId
+
+	      room.onMessage("commencerJeu", _message => {
+	        self.commencerJeu()
+	      });
+
+	      room.onStateChange((changes: any) => {
+	        let joueursPresents = {}
+	        let contenu: any = []
+
+	        changes.joueurs.forEach((value: any, key: any) => {
+					//@ts-ignore
+	          joueursPresents[key] = value
+	        })
+
+	        changes.listeJoueurIndex.forEach((listeID: string, idx: any) => {
+	          let obj = JSON.parse(listeID)
+	          for (const [key, value] of Object.entries(obj[idx])) {
+	            if (value) {
+					//@ts-ignore
+	              this.listeJoueur[key].setText(value)
+	            } else {
+					//@ts-ignore
+	              this.listeJoueur[key].setText('')
+	            }
+	          }
+	        })
+
+	        Object.keys(joueursPresents).map(val => {
+					//@ts-ignore
+	          contenu.push(val.concat(`${joueursPresents[val].pret ? '  ✅ PRET !' : ' 🔴 CHOIX EN COURS...'} ${changes.proprietaire[0] == val ? '👑' : ''}`))
+	        })
+
+	        self.panelGauche.setTitre(`Joueurs : ${Object.keys(joueursPresents).length} / 4`)
+					//@ts-ignore
+	        const tout_le_monde_est_pret = Object.keys(joueursPresents).filter((item: any) => joueursPresents[item].pret == true).length == Object.keys(joueursPresents).length; // 6
+
+	        if (tout_le_monde_est_pret)  {
+
+	          if (changes.proprietaire[0] == self.session) {
+	            self.boutonActivable = true;
+	            self.bouton.setText('Commencer la partie !')
+	        } else {
+	          self.boutonActivable = false;
+	          self.bouton.setText('Le proprietaire 👑 peut commencer la partie !')
+	        }
+
+	      } else {
+	        self.bouton.setText("Un joueur n'est pas prêt !")
+	        self.boutonActivable = false;
+	      }
+	        self.panelGauche.setContenu(contenu)
+	      })
+	    })
+	    .catch((err) => {
+	      console.error(err)
+	    })
+	  }
+
+	  commencerJeu() {
+	    this.room.leave()
+	    this.scene.start('Jeu_01', {
+	      salon: this.salon,
+	      personnage: this.personnageChoisie
+	    });
+	  }
+
+	  demandeCommencerJeu() {
+	    this.room.send('demandeCommencerJeu')
+	  }
+
+
+// 	async create() {
+// 		this.editorCreate();
+// 		this.boutonLobby
+// 		.setInteractive({ useHandCursor: true })
+// 		.on('pointerdown', () => {
+// 			this.scene.start('Jeu')
+// 		});
+//
+// 		console.log(`WELCOME TO LOBBY ${this.salon}`)
+//
+// 		await this.connexion()
+// 	}
+//
+// 	async connexion() {
+// 		const self = this
+// 		const client = new Colyseus.Client("ws://localhost:3000")
+// 		const salon = this.salon
+//
+// console.log("LLLLOOOBY")
+// 		await client
+// 		.joinOrCreate("lobby", { salon })
+// 		.then((room) => {
+// 			self.room = room
+// 			self.session = room.sessionId
+//
+// 			setTimeout(() => {
+// 				// self.demandeCommencerJeu()
+// 			}, 2000);
+//
+// 			room.onMessage("commencerJeu", _message => {
+// 				self.commencerJeu()
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			console.error(err)
+// 		})
+// 	}
+//
+// 	commencerJeu() {
+// 		this.room.leave()
+// 		this.scene.start('Jeu', {
+// 			salon: this.salon,
+// 			personnage: this.personnageChoisie
+// 		});
+// 	}
+//
+// 	demandeCommencerJeu() {
+// 		this.room.send('demandeCommencerJeu')
+// 	}
+//
 
 
 	/* END-USER-CODE */
