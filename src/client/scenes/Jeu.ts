56b868238c173e440db97c971969e2087b6417f4
pytoncraft2 <pythoncraft2@gmail.com>
2022-06-25 02:40:54 +0200

    [~] barre de vie dans le panel

--- src/client/scenes/Jeu.ts
+++ src/client/scenes/Jeu.ts
@@ -270,6 +270,13 @@ export default class Jeu extends Phaser.Scene {
 		profilFakhear_1.scaleX = 0.16275708974409425;
 		profilFakhear_1.scaleY = 0.16275708974409425;
 
+		// rectangle_3
+		const rectangle_3 = this.add.rectangle(1042, 1055, 128, 128);
+		rectangle_3.scaleX = 5.150602609693634;
+		rectangle_3.scaleY = 0.21267753602571016;
+		rectangle_3.isFilled = true;
+		rectangle_3.fillColor = 15554289;
+
 		this.map_boss1 = map_boss1;
 		this.map_boss2 = map_boss2;
 		this.hall = hall;
