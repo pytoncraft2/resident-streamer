db679098d13c134d3267b8a5f9f686555178bd78
pytoncraft2 <pythoncraft2@gmail.com>
2022-07-10 01:59:56 +0200

    [🏃] troll qui court

--- src/server/game/scenes/Aptitudes/troll.ts
+++ src/server/game/scenes/Aptitudes/troll.ts
@@ -1,1 +1,48 @@
-export function __StatsSupplementaire() {}
+import TJoueur from "../types/Joueur";
+
+export function __StatsSupplementaire(personnage: TJoueur, Aptitudes: any) {
+  personnage.anims.create({
+    key: 'troll_run',
+    frames: personnage.anims.generateFrameNames('atlas', { prefix: 'trollrun', start: 0, end: 5 }),
+    frameRate: 30,
+    repeat: 0
+  });
+
+  personnage.anims.create({
+    key: 'troll_idle',
+    frames: personnage.anims.generateFrameNames('atlas', { prefix: 'troll', start: 0, end: 0 }),
+    frameRate: 1,
+    repeat: 0
+  });
+
+
+  Aptitudes[personnage.sprite].toucheEspace = (personnage: Phaser.Physics.Arcade.Sprite, _input: any) => {}
+  Aptitudes[personnage.sprite].toucheDroite = (personnage: Phaser.Physics.Arcade.Sprite, _input: any) => {
+    if (_input.right_fin)
+    {
+      personnage.play('troll_idle')
+      personnage.setVelocityX(0)
+    }
+    else
+    {
+      personnage.play('troll_run', true)
+      personnage.setVelocityX(1800)
+      if (personnage.flipX) personnage.setFlipX(false)
+    }
+  }
+
+  Aptitudes[personnage.sprite].toucheGauche = (personnage: Phaser.Physics.Arcade.Sprite, _input: any) => {
+    if (_input.left_fin)
+    {
+      personnage.play('troll_idle')
+      personnage.setVelocityX(0)
+    }
+    else
+    {
+      personnage.play('troll_run', true)
+      personnage.setVelocityX(-1800)
+      if (!personnage.flipX) personnage.setFlipX(true)
+    }
+  }
+
+}
