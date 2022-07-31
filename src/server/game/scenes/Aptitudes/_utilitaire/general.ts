a3d46943feed816b54467ebc586a4660d34b8c81
pytoncraft2 <pythoncraft2@gmail.com>
2022-07-31 17:32:02 +0200

    [~] test avec fonction closest

--- src/server/game/scenes/Aptitudes/_utilitaire/general.ts
+++ src/server/game/scenes/Aptitudes/_utilitaire/general.ts
@@ -1,6 +1,18 @@
 export function fusion(personnage: any, input: any) {
   if (input.tab)
   {
+    const ennemieProche = closest(personnage, 'enemies');
+    if (ennemieProche)
+    {
+      if ((personnage.scene as any).room.boss[`${ennemieProche.sprite}`].vaincu)
+      {
+        //Fusion possible
+      }
+      else
+      {
+        //recuperation objet boss possible
+      }
+    }
     input.tab = false;
   }
   if (input.tab_fin)
