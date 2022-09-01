a3929608e73677fdc7ecea198cd3b2b17f3bc302
pytoncraft2 <pythoncraft2@gmail.com>
2022-09-01 22:19:00 +0200

    [-] https

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,7 @@ import { WebSocketTransport } from "@colyseus/ws-transport"
 import { Server } from "colyseus"
 import dotenv from "dotenv"
 import express from "express"
-import https from "https"
+import http from "http"
 import path from "path"
 import { monitor } from "@colyseus/monitor";
 import * as fs from 'fs';
@@ -76,10 +76,7 @@ app.get('/:id', (_request, response) => {
 
 
 // Define game server
-const server = https.createServer({
-      key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert"),
-    },app)
+const server = http.createServer(app)
 const gameServer = new Server({
   transport: new WebSocketTransport({
     server: server,
