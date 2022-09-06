99208fd9b80b7b0936e969aac40eeb16ee4918b2
pytoncraft2 <pythoncraft2@gmail.com>
2022-09-06 15:17:49 +0200

    [+] script sw.js

--- static/pwa.js
+++ static/pwa.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', init, false);
 function init() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/favicons/sw.js')
+    navigator.serviceWorker.register('/sw.js')
       .then((reg) => {
         console.log('Service worker registered -->', reg);
       }, (err) => {
